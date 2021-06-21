872f142b5b8a02f72f9b90eeb14eae44b0f4aec4
juanbernalGL <juan.bernal@gorillalogic.com>
2021-06-21 13:19:53 -0500

    Included gsap animation and layout component

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -7,6 +7,9 @@ import "../styles/layout.css";
 import * as styles from "./layout.module.css";
 
 import favicon from "../assets/images/favicon.ico";
+import ZeroanimaLogo from "../assets/svg/Zeroanima_Logo_red.svg";
+import ZeroanimaLogoSecondary from "../assets/images/Zeroanima_Logo_white_small.png";
+import Designed from "../assets/svg/Designed.svg";
 import Helmet from "react-helmet";
 
 const Layout = ({ children, onHideNav, onShowNav, showNav, siteTitle }) => (
@@ -14,12 +17,6 @@ const Layout = ({ children, onHideNav, onShowNav, showNav, siteTitle }) => (
     <Helmet>
       <link rel="icon" href={favicon} />
     </Helmet>
-    <Header
-      siteTitle={siteTitle}
-      onHideNav={onHideNav}
-      onShowNav={onShowNav}
-      showNav={showNav}
-    />
     <div className="w-8/12 lg:10/12 flex flex-col">
       <Navbar />
       <Banner />
@@ -33,6 +30,24 @@ const Layout = ({ children, onHideNav, onShowNav, showNav, siteTitle }) => (
           </div>
         </div>
       </footer>
+    </div>
+    <div className="w-4/12 lg:2/12 border-l-2 border-b-2 border-primary flex flex-col">
+      <div className="h-80 flex justify-center static">
+        <img src={ZeroanimaLogo} alt="ZeroanimaLogo" className="" />
+      </div>
+
+      <div className="h-8 border-b-2 border-primary left-0">
+        <p className="text-2xl pl-1 tracking-wider font-normal">SINCE</p>
+      </div>
+      <div>
+        <p className="text-2xl pl-1 tracking-wider font-normal">1985</p>
+      </div>
+      <div className="flex justify-center pt-16">
+        <img src={ZeroanimaLogoSecondary} alt="ZeroanimaLogoSecondary" />
+      </div>
+      <div className="flex justify-center pt-16">
+        <img src={Designed} alt="Designed" />
+      </div>
     </div>
   </div>
 );
